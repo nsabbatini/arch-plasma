d62884b153ae391d91dee06112055ed19074ee5d
nsabbatini <Narcizo.Sabbatini@gmail.com>
2024-06-20 10:45:30 -0300

    Improved instructions

--- etc/NetworkManager/dispatcher.d/70-bridge-down.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-interface=$1
-action=$2
-
-[[ $interface == "enp0s20f0u1" && $action == "down" ]] && { nmcli connection down bridge-br0; exit 0; }
-[[ $interface == "enp0s20f0u6u3" && $action == "down" ]] && { nmcli connection down bridge-br0; exit 0; }
-
